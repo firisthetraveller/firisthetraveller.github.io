3cdbb46e5b3a15f65e527b9578ea9bf93ac6ebe7
Guy Luong <16323642+firisthetraveller@users.noreply.github.com>
2025-12-04 02:49:47 +0100

    [Icons] Show in "Powered by"

--- app/routes/projects.jsx
+++ app/routes/projects.jsx
@@ -6,18 +6,24 @@ const Projects = () => {
     const { data, isPending } = useCollection("projects");
 
     return !isPending && (
-        <main className="projects">
-            {data && data.map((p, i) => (
-                <Link key={i} to={`/projects/${p.id}`}>
-                    <div className="parent">
-                        <div className="child">
-                            <h3>{localeText(p.title)}</h3>
-                            <p>{localeText(p.description)}</p>
+        <main className="flex flex-col gap-4">
+            <div>
+                <h3>Projects</h3>
+                <p>A showcase of different projects I've done.</p>
+            </div>
+            <div className="projects">
+                {data && data.map((p, i) => (
+                    <Link key={i} to={`/projects/${p.id}`}>
+                        <div className="parent">
+                            <div className="child">
+                                <h3>{localeText(p.title)}</h3>
+                                <p>{localeText(p.description)}</p>
+                            </div>
+                            {p.images ? <img src={p.images[0]} /> : <p>No image available</p>}
                         </div>
-                        {p.images ? <img src={p.images[0]} /> : <p>No image available</p>}
-                    </div>
-                </Link>
-            ))}
+                    </Link>
+                ))}
+            </div>
         </main>
     );
 }
--- app/routes/projects/ProjectDetails.jsx
+++ app/routes/projects/ProjectDetails.jsx
@@ -2,7 +2,7 @@ import { useLoaderData } from "react-router";
 import { localeText } from "../../helpers/lang";
 import getDocument from "../../hooks/getDocument";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faNode, faPython, faReact, faVuejs } from "@fortawesome/free-brands-svg-icons";
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 export async function clientLoader({ params }) {
@@ -10,6 +10,28 @@ export async function clientLoader({ params }) {
     const details = await getDocument("project-details", params.id);
     return { infos, details };
 }
+
+const Icon = ({ name, icon, faIcon, noname = false }) => {
+    return (
+        <div className="flex items-center cursor-pointer min-h-11 gap-1">
+            {icon && <img src={icon} alt={name} width={noname ? 32 * 3 : 32} height={32} aria-label={`icon for ${icon}`} />}
+            {faIcon && <FontAwesomeIcon icon={faIcon} size="2x" />}
+            {!noname && <p>{name}</p>}
+        </div>
+    )
+}
+
+const skills = {
+    "React": { name: "React", faIcon: faReact },
+    "React Native": { name: "React Native", faIcon: faReact },
+    "Python": { name: "Python", faIcon: faPython },
+    "Node": { name: "Node", faIcon: faNode },
+    "Firebase": { name: "Firebase", icon: `/assets/icons/firebase.svg`, noname: true },
+    "VueJS": { name: "VueJS", faIcon: faVuejs },
+    "Tailwind": { name: "Tailwind", icon: `/assets/icons/tailwind.svg`, noname: true },
+}
+
+const hidden = ["Mentoring"]
 
 const ProjectDetails = () => {
     let { infos, details } = useLoaderData();
@@ -28,8 +50,8 @@ const ProjectDetails = () => {
             {/** Tags - Powered by */}
             <div className="flex flex-col">
                 <p>Powered by:</p>
-                <div className="flex gap-4">
-                    {infos.tags.map(t => <span>{t}</span>)}
+                <div className="flex items-center gap-4">
+                    {infos.tags.filter(t => !hidden.includes(t)).map(t => (skills[t] ? <Icon name={skills[t].name} icon={skills[t].icon} faIcon={skills[t].faIcon} noname={skills[t].noname} /> : <span>{t}</span>))}
                 </div>
             </div>
         </main>
